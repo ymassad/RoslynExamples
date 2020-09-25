57eb952515e5545b4eafb560cb4adc70810cca77
Yacoub Massad <yacoubmassad@gmail.com>
2020-09-25 02:50:48 -0700

    Initial commit for "Rolsyn Analyzer and CodeFix deployment options"

---
+++ Roslyn Analyzer and CodeFix deployment options/AnalyzerProject/CreationCodeFixProvider.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Composition;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace AnalyzerProject
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(CreationCodeFixProvider))]
+    [Shared]
+    public class CreationCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds =>
+            ImmutableArray.Create("BadWayOfCreatingImmutableArray");
+
+        //System.Collections.Immutable.ImmutableArray<int>.Empty.Add(1);
+
+        //ImmutableArray<int>.Empty.Add(1);
+        //System.Collections.Immutable.ImmutableArray.Create(1);
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+
+            var document = context.Document;
+
+            var root = await document.GetSyntaxRootAsync();
+
+            var node = root.FindNode(diagnostic.Location.SourceSpan);
+
+            if (!(node is InvocationExpressionSyntax addInvocation))
+                throw new Exception("Expected node to be of type InvocationExpressionSyntax");
+
+            if(!(addInvocation.Expression is MemberAccessExpressionSyntax addMemberAccess))
+                throw new Exception("Expected addInvocation.Expression to be of type MemberAccessExpressionSyntax");
+
+            if(!(addMemberAccess.Expression is MemberAccessExpressionSyntax emptyMemberAccess))
+                throw new Exception("Expected addMemberAccess.Expression to be of type MemberAccessExpressionSyntax");
+
+            context.RegisterCodeFix(CodeAction.Create("Use ImmutableArray.Create", async c =>
+            {
+                var noNeedToUseFullnamespace =
+                    emptyMemberAccess.Expression is GenericNameSyntax;
+
+                var argument = addInvocation.ArgumentList.Arguments.Single();
+
+                var semanticModel = await document.GetSemanticModelAsync();
+
+                var argumentTypeInfo = semanticModel.GetTypeInfo(argument.Expression);
+
+                var argumentTypeWasConverted =
+                    !SymbolEqualityComparer.Default
+                        .Equals(argumentTypeInfo.Type, argumentTypeInfo.ConvertedType);
+
+                var createMethodAccessExpr = CreateMemberAccessExpression(
+                    noNeedToUseFullnamespace
+                        ? "ImmutableArray.Create"
+                        : "System.Collections.Immutable.ImmutableArray.Create");
+
+                if (argumentTypeWasConverted)
+                {
+                    var syntaxGenerator = SyntaxGenerator.GetGenerator(document);
+
+                    TypeSyntax elementTypeSyntax = (TypeSyntax)syntaxGenerator.TypeExpression(
+                        argumentTypeInfo.ConvertedType);
+
+                    createMethodAccessExpr =
+                        SyntaxFactory.MemberAccessExpression(
+                            SyntaxKind.SimpleMemberAccessExpression,
+                            createMethodAccessExpr.Expression,
+                            SyntaxFactory.GenericName(
+                                SyntaxFactory.Identifier("Create"),
+                                SyntaxFactory.TypeArgumentList(
+                                    SyntaxFactory.SingletonSeparatedList(elementTypeSyntax))));
+                }
+
+                var updatedRoot = root.ReplaceNode(addInvocation.Expression, createMethodAccessExpr);
+
+                return document.WithSyntaxRoot(updatedRoot);
+            }), diagnostic);
+        }
+
+        private static MemberAccessExpressionSyntax CreateMemberAccessExpression(string accessExpression)
+        {
+            var parts = accessExpression.Split('.');
+
+            if (parts.Length < 2)
+                throw new Exception("There should at least be two parts");
+
+            var memberAccessExpression =
+                SyntaxFactory.MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                        SyntaxFactory.IdentifierName(parts[0]),
+                        SyntaxFactory.IdentifierName(parts[1]));
+
+
+            for(int i = 2; i < parts.Length; i++)
+            {
+                memberAccessExpression =
+                    SyntaxFactory.MemberAccessExpression(
+                        SyntaxKind.SimpleMemberAccessExpression,
+                            memberAccessExpression,
+                            SyntaxFactory.IdentifierName(parts[i]));
+            }
+
+            return memberAccessExpression;
+
+        }
+    }
+}
---
+++ Roslyn Analyzer and CodeFix deployment options/TestProject/CodeFixProviderTests.cs
@@ -0,0 +1,138 @@
+﻿using AnalyzerProject;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class CodeFixProviderTests
+    {
+        [TestMethod]
+        public async Task CreatingAnImmutableArrayViaTheEmptyPropertyWithoutOpeningTheImmutableNamespace_CodeFixMakesTheCodeUseTheCreateMethod()
+        {
+            var code = @"
+public static class Program
+{
+    public static void Main()
+    {
+        var array = System.Collections.Immutable.ImmutableArray<int>.Empty.Add(1);
+    }
+}";
+
+            var expectedChangedCode = @"
+public static class Program
+{
+    public static void Main()
+    {
+        var array = System.Collections.Immutable.ImmutableArray.Create(1);
+    }
+}";
+            await RunTest(code, expectedChangedCode);
+        }
+
+        [TestMethod]
+        public async Task CreatingAnImmutableArrayViaTheEmptyPropertyWithoutUsingFullNameOfImmutableArrayType_CodeFixMakesTheCodeUseTheCreateMethod()
+        {
+            var code = @"
+using System.Collections.Immutable;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var array = ImmutableArray<int>.Empty.Add(1);
+    }
+}";
+
+            var expectedChangedCode = @"
+using System.Collections.Immutable;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var array = ImmutableArray.Create(1);
+    }
+}";
+            await RunTest(code, expectedChangedCode);
+        }
+
+        [TestMethod]
+        public async Task CreatingAnImmutableArrayViaTheEmptyProperty_ArrayTypeIsLongButValueTypeIsInt_CodeFixMakesTheCodeUseTheCreateMethod_CorrectArrayTypeIsCreated()
+        {
+            var code = @"
+using System.Collections.Immutable;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var array = ImmutableArray<long>.Empty.Add(1);
+    }
+}";
+
+            var expectedChangedCode = @"
+using System.Collections.Immutable;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var array = ImmutableArray.Create<long>(1);
+    }
+}";
+            await RunTest(code, expectedChangedCode);
+        }
+
+
+        private static async Task RunTest(string code, string expectedChangedCode)
+        {
+            var (diagnostics, document, workspace) = await Utilities.GetDiagnosticsAdvanced(code);
+
+            Assert.AreEqual(1, diagnostics.Length);
+
+            var diagnostic = diagnostics[0];
+
+            var codeFixProvider = new CreationCodeFixProvider();
+
+            CodeAction registeredCodeAction = null;
+
+            var context = new CodeFixContext(document, diagnostic, (codeAction, _) =>
+            {
+                if (registeredCodeAction != null)
+                    throw new Exception("Code action was registered more than once");
+
+                registeredCodeAction = codeAction;
+
+            }, CancellationToken.None);
+
+            await codeFixProvider.RegisterCodeFixesAsync(context);
+
+            if (registeredCodeAction == null)
+                throw new Exception("Code action was not registered");
+
+            var operations = await registeredCodeAction.GetOperationsAsync(CancellationToken.None);
+
+            foreach (var operation in operations)
+            {
+                operation.Apply(workspace, CancellationToken.None);
+            }
+
+            var updatedDocument = workspace.CurrentSolution.GetDocument(document.Id);
+
+
+            var newCode = (await updatedDocument.GetTextAsync()).ToString();
+
+            Assert.AreEqual(expectedChangedCode, newCode);
+        }
+    }
+}
